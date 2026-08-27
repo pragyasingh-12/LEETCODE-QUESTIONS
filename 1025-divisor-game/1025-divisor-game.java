40877e351ae422c7df5a83d9e2bb42edf7a7af0f
pragyasingh-12 <pragya.16702@gmail.com>
2026-08-28 02:42:56 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.9%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n>0 && n%2==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
