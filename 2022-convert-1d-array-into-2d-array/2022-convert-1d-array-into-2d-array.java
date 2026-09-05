235e1406218552439179fb2703aa3fe0adb78036
pragyasingh-12 <pragya.16702@gmail.com>
2026-09-05 12:47:35 +0530

    Time: 4 ms (50.39%), Space: 72.9 MB (5.95%) - LeetHub

---
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int[][] newmat = new int[m][n];
+        int k=0;
+        if((m*n)!=original.length)
+        {
+            return new int[0][0];
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                newmat[i][j]=original[k];
+                k++;
+            }
+        }
+        return newmat;
+    }
+}
