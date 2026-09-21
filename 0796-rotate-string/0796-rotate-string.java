0e8ed049899ff1c68cb782628dd8ac32e180e25c
pragyasingh-12 <pragya.16702@gmail.com>
2026-09-21 20:23:33 +0530

    Time: 1 ms (98.38%), Space: 43.5 MB (50.04%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,6 +1,15 @@
 import java.util.Arrays;
 
 class Solution {
+    private void rotateLeft(char[] arr) 
+    {
+        char first = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) 
+        {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = first;
+    }
     public boolean rotateString(String s, String goal) {
         if (s.length() != goal.length()) 
         {
@@ -17,15 +26,5 @@ class Solution {
             rotateLeft(ch1);
         }
         return false;
-    }
-    
-    private void rotateLeft(char[] arr) 
-    {
-        char first = arr[0];
-        for (int i = 0; i < arr.length - 1; i++) 
-        {
-            arr[i] = arr[i + 1];
-        }
-        arr[arr.length - 1] = first;
     }
 }
