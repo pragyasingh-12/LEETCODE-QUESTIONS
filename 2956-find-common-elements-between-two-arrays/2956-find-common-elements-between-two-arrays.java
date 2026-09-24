3cd13b57604968d2e5c035a6faeba88431071e48
pragyasingh-12 <pragya.16702@gmail.com>
2026-09-25 00:17:32 +0530

    Time: 5 ms (82.02%), Space: 47.4 MB (34.73%) - LeetHub

---
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        int count=0;
+        int count2=0;
+        for(int i=0;i<nums1.length;i++)
+        {
+            for(int j=0;j<nums2.length;j++)
+            {
+              if(nums1[i]==nums2[j])
+              {
+                count++;
+                break;
+              }
+            }
+        }
+        for(int i=0;i<nums2.length;i++)
+        {
+            for(int j=0;j<nums1.length;j++)
+            {
+                if(nums2[i]==nums1[j])
+                {
+                    count2++;
+                    break;
+                }
+            }
+        }
+        return new int[] {count, count2};
+    }
+}
